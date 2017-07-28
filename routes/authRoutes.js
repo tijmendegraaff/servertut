f0a2d9d6f5365bd1203b52202c18d85703d67cad
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-28 22:21:03 +0200

    add current user route after sign in

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,4 +11,8 @@ module.exports = app => {
 
   // setup Auth callback route
   app.get("/auth/google/callback", passport.authenticate("google"));
+
+  app.get("/api/current_user", (req, res) => {
+    res.send(req.user);
+  });
 };
