f9e8609dad7d9097a44e947d0be5c12f32ed4336
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-28 18:07:36 +0200

    set google auth strategy

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./config/keys");
 
 const app = express();
 
 // https://console.developers.google.com
-passport.use(new GoogleStrategy());
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret
+  })
+);
 
+// server Config
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
