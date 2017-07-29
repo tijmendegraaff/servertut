4d5429326c08b921a7191b0f52754ee823347325
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-29 23:51:25 +0200

    refactor with async syntax

--- services/passport.js
+++ services/passport.js
@@ -24,16 +24,14 @@ passport.use(
       callbackURL: "/auth/google/callback",
       proxy: true
     },
-    (accesToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          done(null, existingUser);
-        } else {
-          new User({ googleId: profile.id })
-            .save()
-            .then(user => done(null, user));
-        }
-      });
+    async (accesToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+      if (existingUser) {
+        done(null, existingUser);
+      } else {
+        const user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+      }
     }
   )
 );
