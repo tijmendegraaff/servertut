fb88ef2be311f9deebcbde52aef4d9b9ec6558b5
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-30 03:10:12 +0200

    minor fix

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,5 +4,5 @@ import { FETCH_USER } from "./types";
 // check to see if user is logged in
 export const fetchUser = () => async dispatch => {
   const res = await axios.get("/api/current_user");
-  dispatch({ type: FETCH_USER, payload: res });
+  dispatch({ type: FETCH_USER, payload: res.data });
 };
