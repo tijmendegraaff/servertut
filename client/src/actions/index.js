c3810902a726426f45b6c54dbff537153258089a
Tijmen de Graaff <tijmen76@gmail.com>
2017-07-30 03:06:58 +0200

    refactor fetch user dispatch function

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,10 +1,8 @@
-import Axios from "axios";
+import axios from "axios";
 import { FETCH_USER } from "./types";
 
-const fetchUser = () => {
-  return function(dispatch) {
-    axios
-      .get("/api/current_user")
-      .then(res => dispatch({ FETCH_USER, payload: res }));
-  };
+// check to see if user is logged in
+export const fetchUser = () => async dispatch => {
+  const res = await axios.get("/api/current_user");
+  dispatch({ type: FETCH_USER, payload: res });
 };
