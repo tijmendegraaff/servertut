11ea96466dba92a2016de15219e446bc393c3981
Tijmen de Graaff <tijmen76@gmail.com>
2017-08-04 22:59:17 +0200

    add billing to backend with error

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose; // const Schema = mongoose.Schema; is the same in ES6 as
 
 const userSchema = new Schema({
-  googleId: String
+  googleId: String,
+  credits: {
+    type: number,
+    default: 0
+  }
 });
 
 mongoose.model("users", userSchema);
