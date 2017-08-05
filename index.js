806a5ae830f2d79acabd6385271b876ddefab6d6
Tijmen de Graaff <tijmen76@gmail.com>
2017-08-05 17:08:32 +0200

    config routes for deployment

--- index.js
+++ index.js
@@ -32,6 +32,16 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // express will serve up production assets
+  app.user(express.static("client/build"));
+  // express will serve up index.html fil if it doesn't recognize the route
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 // server Config
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
