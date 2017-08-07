246ae04918f0a3349b4dc699c0c06db2649f5f4d
Tijmen de Graaff <tijmen76@gmail.com>
2017-08-07 18:57:22 +0200

    problems with webhook

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const Path = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -12,8 +15,12 @@ module.exports = app => {
   });
 
   app.post("/api/surveys/webhooks", (req, res) => {
-    console.log(req.body);
-    res.send({});
+    const events = _.map(req.body, event => {
+      console.log(req.body);
+      const pathname = new URL(event.url).pathname;
+      const p = new Path("/api/suveys/:surveyId/:choice");
+      // console.log(p.test(pathname));
+    });
   });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
