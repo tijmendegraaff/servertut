1e6df513aa6dfa9eb42aa252028654bb33c04dc6
Tijmen de Graaff <tijmen76@gmail.com>
2017-08-09 16:13:30 +0200

    updated query and save of survey model

--- models/Survey.js
+++ models/Survey.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose; // const Schema = mongoose.Schema; is the same in ES6 as
+const { Schema } = mongoose;
 const RecipientSchema = require("./Recipient");
 
 const surveySchema = new Schema({
@@ -9,10 +9,7 @@ const surveySchema = new Schema({
   recipients: [RecipientSchema],
   yes: { type: Number, default: 0 },
   no: { type: Number, default: 0 },
-  _user: {
-    type: Schema.Types.ObjectId,
-    ref: "User"
-  },
+  _user: { type: Schema.Types.ObjectId, ref: "User" },
   dateSent: Date,
   lastResponded: Date
 });
