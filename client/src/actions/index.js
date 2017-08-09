b082d853a3d4a1dca9cf1d3be6b675c4be924b9c
Tijmen de Graaff <tijmen76@gmail.com>
2017-08-09 16:41:06 +0200

    add surveys to redux state

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { FETCH_USER } from "./types";
+import { FETCH_SURVEYS } from "./types";
 
 // check to see if user is logged in
 export const fetchUser = () => async dispatch => {
@@ -16,4 +17,9 @@ export const submitSurvey = (values, history) => async dispatch => {
   const res = await axios.post("/api/surveys", values);
   history.push("/surveys");
   dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+  const res = await axios.get("/api/surveys");
+  dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
